6a88d5a2eb1f8563d5bf26ea594e4c6314d0222a
Jakub Pruszyski <jakub.pruszynski@pragmaticcoders.com>
2026-03-21 19:04:37 +0100

    commit

--- src/types/tool.ts
+++ src/types/tool.ts
@@ -3,5 +3,5 @@ import type { Document } from "./document.ts";
 export interface ToolDefinition {
   name: string;
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  handler: (args: any) => Promise<Document | Document[]>;
+  handler: (args: Record<string, any>) => Promise<Document | Document[]>;
 }
